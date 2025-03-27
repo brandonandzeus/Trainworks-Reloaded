0f860e7a3891100acb3381b5df79c03f012a8eb8
Brandon <tricksterguy87@gmail.com>
2025-03-26 17:35:38 -0700

    As so commanded by ShinySteve. DisableErrorReporting

---
+++ TrainworksReloaded.Plugin/Patches/DisableErrorReportingPatch.cs
@@ -0,0 +1,17 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TrainworksReloaded.Plugin.Patches
+{
+    // Prevent sending error reports to ShinyShoe.
+    [HarmonyPatch(typeof(ShinyShoe.AppManager), "DoesThisBuildReportErrors")]
+    class DisableErrorReportingPatch
+    {
+        public static void Postfix(ref bool __result)
+        {
+            __result = false;
+        }
+    }
+}
