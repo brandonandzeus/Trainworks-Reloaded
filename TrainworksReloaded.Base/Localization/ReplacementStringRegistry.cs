bdb1d2f74fd3272c345f8c5d550ffb7a7f36630b
Brandon <tricksterguy87@gmail.com>
2025-04-18 14:43:27 -0700

    Fix build error.

--- TrainworksReloaded.Base/Localization/ReplacementStringRegistry.cs
+++ TrainworksReloaded.Base/Localization/ReplacementStringRegistry.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using System.Text;
+using TrainworksReloaded.Core.Enum;
 using TrainworksReloaded.Core.Interfaces;
 
 namespace TrainworksReloaded.Base.Localization
@@ -43,22 +44,29 @@ namespace TrainworksReloaded.Base.Localization
             }
         }
 
-        public bool TryLookupName(
-            string name,
-            [NotNullWhen(true)] out ReplacementStringData? lookup,
-            [NotNullWhen(true)] out bool? IsModded
-        )
+        public List<string> GetAllIdentifiers(RegisterIdentifierType identifierType)
         {
-            throw new NotImplementedException();
+            return identifierType switch
+            {
+                RegisterIdentifierType.ReadableID => [.. this.Keys],
+                RegisterIdentifierType.GUID => [.. this.Keys],
+                _ => []
+            };
         }
 
-        public bool TryLookupId(
-            string id,
-            [NotNullWhen(true)] out ReplacementStringData? lookup,
-            [NotNullWhen(true)] out bool? IsModded
-        )
+        public bool TryLookupIdentifier(string identifier, RegisterIdentifierType identifierType, [NotNullWhen(true)] out ReplacementStringData? lookup, [NotNullWhen(true)] out bool? IsModded)
         {
-            throw new NotImplementedException();
+            lookup = null;
+            IsModded = true;
+            switch (identifierType)
+            {
+                case RegisterIdentifierType.ReadableID:
+                    return this.TryGetValue(identifier, out lookup);
+                case RegisterIdentifierType.GUID:
+                    return this.TryGetValue(identifier, out lookup);
+                default:
+                    return false;
+            }
         }
     }
 }
