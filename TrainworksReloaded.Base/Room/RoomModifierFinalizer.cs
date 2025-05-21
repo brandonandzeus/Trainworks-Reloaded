e02c04c9f467809d1d7dbc207a042ff1d118eab8
Brandon <tricksterguy87@gmail.com>
2025-05-20 23:05:32 -0700

    room modifier updates.
    s/"play_descriptions"/"in_play_descriptions"/g
    s/"use_name_as_description"/"use_name_for_description"/g
    s/effects/param_effects
    s/trigger/param_trigger

--- TrainworksReloaded.Base/Room/RoomModifierFinalizer.cs
+++ TrainworksReloaded.Base/Room/RoomModifierFinalizer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using HarmonyLib;
 using TrainworksReloaded.Base.Card;
@@ -7,6 +8,7 @@ using TrainworksReloaded.Base.Extensions;
 using TrainworksReloaded.Core.Extensions;
 using TrainworksReloaded.Core.Interfaces;
 using UnityEngine;
+using static TrainworksReloaded.Base.Extensions.ParseReferenceExtensions;
 
 namespace TrainworksReloaded.Base.Room
 {
@@ -73,11 +75,11 @@ namespace TrainworksReloaded.Base.Room
                 $"Finalizing Room Modifier {definition.Id.ToId(key, "RoomModifier")}... "
             );
 
-            var cardConfig = configuration.GetSection("param_card").Value;
+            var cardReference = configuration.GetSection("param_card").ParseReference();
             if (
-                cardConfig != null
+                cardReference != null
                 && cardDataRegister.TryLookupName(
-                    cardConfig.ToId(key, TemplateConstants.Card),
+                    cardReference.ToId(key, TemplateConstants.Card),
                     out var cardData,
                     out var _
                 )
@@ -88,11 +90,11 @@ namespace TrainworksReloaded.Base.Room
                     .SetValue(data, cardData);
             }
 
-            var param_card_upgrade = configuration.GetSection("param_upgrade").ParseString();
+            var upgradeReference = configuration.GetSection("param_upgrade").ParseReference();
             if (
-                param_card_upgrade != null
+                upgradeReference != null
                 && upgradeDataRegister.TryLookupId(
-                    param_card_upgrade.ToId(key, "Upgrade"),
+                    upgradeReference.ToId(key, TemplateConstants.Upgrade),
                     out var upgradeLookup,
                     out var _
                 )
@@ -103,11 +105,11 @@ namespace TrainworksReloaded.Base.Room
                     .SetValue(data, upgradeLookup);
             }
 
-            var trigered_vfx = configuration.GetSection("triggered_vfx").ParseString();
+            var triggeredVFXId = configuration.GetSection("triggered_vfx").ParseReference()?.ToId(key, TemplateConstants.Vfx);
             if (
-                trigered_vfx != null
+                triggeredVFXId != null
                 && vfxRegister.TryLookupId(
-                    trigered_vfx.ToId(key, "Vfx"),
+                    triggeredVFXId,
                     out var vfxLookup,
                     out var _
                 )
@@ -119,19 +121,14 @@ namespace TrainworksReloaded.Base.Room
             }
 
             var cardEffectDatas = new List<CardEffectData>();
-            var cardEffectDatasConfig = configuration.GetSection("effects").GetChildren();
-            foreach (var configEffect in cardEffectDatasConfig)
+            var effectReferences = configuration.GetSection("param_effects")
+               .GetChildren()
+               .Select(x => x.ParseReference())
+               .Where(x => x != null)
+               .Cast<ReferencedObject>();
+            foreach (var reference in effectReferences)
             {
-                if (configEffect == null)
-                {
-                    continue;
-                }
-                var idConfig = configEffect.GetSection("id").Value;
-                if (idConfig == null)
-                {
-                    continue;
-                }
-                var id = idConfig.ToId(key, "Effect");
+                var id = reference.ToId(key, TemplateConstants.Effect);
                 if (cardEffectDataRegister.TryLookupId(id, out var card, out var _))
                 {
                     cardEffectDatas.Add(card);
@@ -147,20 +144,18 @@ namespace TrainworksReloaded.Base.Room
             List<StatusEffectStackData> paramStatusEffects = [];
             foreach (var child in configuration.GetSection("param_status_effects").GetChildren())
             {
-                var idConfig = child?.GetSection("status").Value;
-                if (idConfig == null)
+                var statusReference = child.GetSection("status").ParseReference();
+                if (statusReference == null)
                     continue;
-                var statusEffectId = idConfig.ToId(key, TemplateConstants.StatusEffect);
-                string statusId = idConfig;
+                var statusEffectId = statusReference.ToId(key, TemplateConstants.StatusEffect);
                 if (statusRegister.TryLookupId(statusEffectId, out var statusEffectData, out var _))
                 {
-                    statusId = statusEffectData.GetStatusId();
+                    paramStatusEffects.Add(new StatusEffectStackData()
+                    {
+                        statusId = statusEffectData.GetStatusId(),
+                        count = child?.GetSection("count").ParseInt() ?? 0,
+                    });
                 }
-                paramStatusEffects.Add(new StatusEffectStackData()
-                {
-                    statusId = statusId,
-                    count = child?.GetSection("count").ParseInt() ?? 0,
-                });
             }
             AccessTools
                 .Field(typeof(RoomModifierData), "paramStatusEffects")
@@ -168,13 +163,12 @@ namespace TrainworksReloaded.Base.Room
 
             //trigger
             var paramTrigger = CharacterTriggerData.Trigger.OnDeath;
-            var triggerSection = configuration.GetSection("trigger");
-            if (triggerSection.Value != null)
+            var triggerReference = configuration.GetSection("trigger").ParseReference();
+            if (triggerReference != null)
             {
-                var value = triggerSection.Value;
                 if (
                     !triggerEnumRegister.TryLookupId(
-                        value.ToId(key, TemplateConstants.CharacterTriggerEnum),
+                        triggerReference.ToId(key, TemplateConstants.CharacterTriggerEnum),
                         out var triggerFound,
                         out var _
                     )
@@ -182,21 +176,17 @@ namespace TrainworksReloaded.Base.Room
                 {
                     paramTrigger = triggerFound;
                 }
-                else
-                {
-                    paramTrigger = triggerSection.ParseTrigger() ?? default;
-                }
             }
             AccessTools
                 .Field(typeof(RoomModifierData), "paramTrigger")
                 .SetValue(data, paramTrigger);
 
             var paramSubtype = "SubtypesData_None";
-            var paramSubtypeId = configuration.GetSection("param_subtype").ParseString();
-            if (paramSubtypeId != null)
+            var subtypeReference = configuration.GetSection("param_subtype").ParseReference();
+            if (subtypeReference != null)
             {
                 if (subtypeRegister.TryLookupId(
-                    paramSubtypeId.ToId(key, TemplateConstants.Subtype),
+                    subtypeReference.ToId(key, TemplateConstants.Subtype),
                     out var lookup,
                     out var _))
                 {
