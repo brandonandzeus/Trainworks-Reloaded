7b9f0ccfe69d4f6aefbbbeab8249dc22d1cc41e1
Brandon <tricksterguy87@gmail.com>
2025-06-09 22:39:44 -0700

    Pipeline/Register/Finalizer for AdditionalTooltipData.

---
+++ TrainworksReloaded.Base/Tooltips/AdditionalTooltipFinalizer.cs
@@ -0,0 +1,83 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Xml.Schema;
+using TMPro;
+using TrainworksReloaded.Base.Extensions;
+using TrainworksReloaded.Core.Extensions;
+using TrainworksReloaded.Core.Interfaces;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.TextCore;
+using static CharacterTriggerData;
+using static UnityEngine.GraphicsBuffer;
+
+namespace TrainworksReloaded.Base.Prefab
+{
+    public class AdditionalTooltipFinalizer : IDataFinalizer
+    {
+        private readonly IModLogger<AdditionalTooltipFinalizer> logger;
+        private readonly ICache<IDefinition<AdditionalTooltipData>> cache;
+        private readonly IRegister<StatusEffectData> statusRegister;
+        private readonly IRegister<CharacterTriggerData.Trigger> triggerEnumRegister;
+
+        public AdditionalTooltipFinalizer(
+            IModLogger<AdditionalTooltipFinalizer> logger,
+            ICache<IDefinition<AdditionalTooltipData>> cache,
+            IRegister<StatusEffectData> statusRegister,
+            IRegister<CharacterTriggerData.Trigger> triggerEnumRegister
+        )
+        {
+            this.logger = logger;
+            this.cache = cache;
+            this.statusRegister = statusRegister;
+            this.triggerEnumRegister = triggerEnumRegister;
+        }
+
+        public void FinalizeData()
+        {
+            foreach (var definition in cache.GetCacheItems())
+            {
+                FinalizeItem(definition);
+            }
+            cache.Clear();
+        }
+
+        public void FinalizeItem(IDefinition<AdditionalTooltipData> definition)
+        {
+            var configuration = definition.Configuration;
+            var data = definition.Data;
+            var key = definition.Key;
+
+            logger.Log(LogLevel.Debug, $"Finalizing AdditionalTooltipData {key} {definition.Id}...");
+
+            data.isTriggerTooltip = false;
+            var triggerReference = configuration.GetSection("trigger").ParseReference();
+            if (triggerReference != null)
+            {
+                var triggerId = triggerReference.ToId(key, TemplateConstants.CharacterTriggerEnum);
+                if (triggerEnumRegister.TryLookupId(triggerId, out var triggerFound, out var _))
+                {
+                    data.isTriggerTooltip = true;
+                    data.trigger = triggerFound;
+                }
+            }
+
+            data.isStatusTooltip = false;
+            var statusReference = configuration.GetSection("status").ParseReference();
+            if (statusReference != null)
+            {
+                var statusEffectId = statusReference.ToId(key, TemplateConstants.StatusEffect);
+                if (statusRegister.TryLookupId(statusEffectId, out var statusEffectData, out var _))
+                {
+                    data.isStatusTooltip = true;
+                    data.statusId = statusEffectData.GetStatusId();
+                }
+            }
+
+        }
+    }
+}
---
+++ TrainworksReloaded.Base/Tooltips/AdditionalTooltipRegister.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using TrainworksReloaded.Core.Enum;
+using TrainworksReloaded.Core.Interfaces;
+using UnityEngine;
+
+namespace TrainworksReloaded.Base.Tooltips
+{
+    public class AdditionalTooltipRegister : Dictionary<string, AdditionalTooltipData>, IRegister<AdditionalTooltipData>
+    {
+        private readonly IModLogger<AdditionalTooltipRegister> logger;
+
+        public AdditionalTooltipRegister(IModLogger<AdditionalTooltipRegister> logger)
+        {
+            this.logger = logger;
+        }
+
+        public void Register(string key, AdditionalTooltipData item)
+        {
+            logger.Log(LogLevel.Debug, $"Register Additional Tooltip {key}");
+            this.Add(key, item);
+        }
+
+
+        public List<string> GetAllIdentifiers(RegisterIdentifierType identifierType)
+        {
+            return identifierType switch
+            {
+                RegisterIdentifierType.ReadableID => [.. this.Keys],
+                RegisterIdentifierType.GUID => [.. this.Keys],
+                _ => [],
+            };
+        }
+
+        public bool TryLookupIdentifier(string identifier, RegisterIdentifierType identifierType, [NotNullWhen(true)] out AdditionalTooltipData? lookup, [NotNullWhen(true)] out bool? IsModded)
+        {
+            lookup = null;
+            IsModded = true;
+            switch (identifierType)
+            {
+                case RegisterIdentifierType.ReadableID:
+                    return this.TryGetValue(identifier, out lookup);
+                case RegisterIdentifierType.GUID:
+                    return this.TryGetValue(identifier, out lookup);
+                default:
+                    return false;
+            }
+        }
+
+    }
+}
